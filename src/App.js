f32ec9c4d3c7db8452ee78f6385f26a2c9ad4fe3
jprzygoda <aprzygodaaa@gmail.com>
2018-07-16 11:42:58 +0200

    Update user added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {connect} from 'react-redux';
+import {updateUser} from './actions/user-actions';
 
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.onUpdateUser=this.onUpdateUser.bind(this);
+    }
+
+    onUpdateUser(){
+        this.props.onUpdateUser('Sammy');
+    }
+
   render() {
-      console.log(this.props)
     return (
       <div className="App">
         <header className="App-header">
@@ -15,13 +25,20 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div onClick={this.onUpdateUser}>Update user</div>
+        {this.props.user}
       </div>
     );
   }
 }
 
-const mapStateToProps=state =>{
-    return state;
+const mapStateToProps=state =>({
+    products: state.products,
+    user: state.user
+});
+
+const mapActionsToProps={
+    onUpdateUser: updateUser
 }
 
-export default connect(mapStateToProps) (App);
+export default connect(mapStateToProps, mapActionsToProps) (App);
