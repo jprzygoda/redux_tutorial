5a28c1c9ed7b9a0aebe2f20c8422407d4dda2dde
jprzygoda <aprzygodaaa@gmail.com>
2018-07-16 11:18:20 +0200

    Created seperate files for reducers

--- src/index.js
+++ src/index.js
@@ -4,23 +4,23 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {combineReducers, createStore} from 'redux';
-
-function productsReducer(state=[], action){
-    return state;
-}
-
-function userReducer(state='', action){
-    return state;
-}
+import {Provider} from 'react-redux';
+import productsReducer from './reducers/products-reducer';
+import userReducer from './reducers/user-reducer';
 
 const allReducers=combineReducers({
     products: productsReducer,
     user: userReducer
 });
 
-const store=createStore(allReducers);
-console.log(store.getState())
+const store=createStore(
+    allReducers,
+    {
+        products: [{name: 'iphone'}],
+        user:'Michael'
+    },
+    window.devToolsExtension && window.devToolsExtension()
+);
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
